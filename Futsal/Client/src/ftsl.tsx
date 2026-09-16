1d7e9c16cd5cd42b291c2e8f1742ded363ceac8f
Mohamed Shazni <shazni.20232600@iit.ac.lk>
2026-09-16 15:10:15 +0530

    Add Admin Dashboard routes to FutsalBooking

--- Futsal/Client/src/ftsl.tsx
+++ Futsal/Client/src/ftsl.tsx
@@ -2,6 +2,7 @@ import HomePage from "./pages/home";
 import ContactPage from "./pages/contact";
 import BookingPage from "./pages/booking";
 import ConfirmationPage from "./pages/confirmation";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import { Route, Routes } from "react-router";
 
 const FutsalBooking = () => {
@@ -14,6 +15,9 @@ const FutsalBooking = () => {
         <Route path="/confirmation" element={<ConfirmationPage />} />
         {/* Support previous /payment route if bookmarked */}
         <Route path="/payment" element={<ConfirmationPage />} />
+        {/* Admin Dashboard */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/*" element={<AdminDashboard />} />
       </Routes>
     </>
   );
