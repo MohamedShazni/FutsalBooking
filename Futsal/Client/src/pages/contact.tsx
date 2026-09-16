3704ff1d57a56b505058d961c4a8fe1dbe25fa47
Mohamed Shazni <shazni.20232600@iit.ac.lk>
2026-09-16 13:17:18 +0530

    Enhance contact form styling and error handling
    Updated input fields to have a transparent background and adjusted error handling to display messages.

--- Futsal/Client/src/pages/contact.tsx
+++ Futsal/Client/src/pages/contact.tsx
@@ -1,9 +1,12 @@
 import { Card, CardContent } from "@/components/ui/card";
 import Image2 from "../assets/4.jpg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const ContactPage = () => {
   const navigate = useNavigate();
+
+  const [error, setError] = useState("");
 
   const handleBack = () => {
     navigate("/");
@@ -21,14 +24,17 @@ const ContactPage = () => {
         state: { customerName: name, customerMobile: mobile },
       });
     } else {
-      alert("Please enter valid details!");
+      setError("Please enter the valid details to book your slots!");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
     }
   };
   return (
     <>
       <div className="min-h-screen bg-[#0c1a25] flex flex-col md:flex-row">
         {/* Left content */}
-        <div className="w-full md:w-1/3 flex flex-col justify-center items-center text-white py-10 md:py-0 order-2 md:order-1">
+        <div className="w-full md:w-2/3 lg:w-1/3 flex flex-col justify-center items-center text-white py-10 md:py-0 order-2 md:order-1">
           <div className="text-center">
             <h1 className="text-4xl md:text-6xl font-bold mb-6 text-white">
               <span className="text-[#00f0ff]">S</span>7{" "}
@@ -45,22 +51,21 @@ const ContactPage = () => {
             </p>
             <div className="mb-5 w-full">
               <form onSubmit={handleSubmit}>
+                {error && <p className="text-red-500 mb-3">{error}</p>}
                 <Card className="bg-[#0c1a25] border-none">
-                  <CardContent>
+                  <CardContent className="w-[400px]">
                     <input
                       type="text"
                       placeholder="Enter Name"
-                      className="w-full bg-white mt-4 p-4 rounded-lg border focus:outline-[#00f0ff]"
-                      required
+                      className="w-full bg-transparent mt-2 p-4 rounded-lg border text-white focus:outline-[#00f0ff]"
                     />
                   </CardContent>
                   <CardContent>
                     <input
                       type="tel"
                       placeholder="Enter Mobile Number"
-                      className="w-full bg-white mt-4 p-4 rounded-lg border focus:outline-[#00f0ff]"
+                      className="w-full bg-transparent mt-2 p-4 rounded-lg border text-white focus:outline-[#00f0ff]"
                       pattern="[0-9]{10}"
-                      required
                     />
                   </CardContent>
                 </Card>
@@ -76,7 +81,7 @@ const ContactPage = () => {
               onClick={handleBack}
               className="bg-[#00f0ff] text-black px-8 py-3 rounded-full text-[18px] font-semibold hover:bg-white hover:text-black hover:border-black transition-all duration-300"
             >
-              Go Back
+              Back to Home
             </button>
           </div>
         </div>
